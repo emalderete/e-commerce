da79e6ac6243af1783498529006c2f64a5288724
emalderete <nicolas.alderete95@live.com>
2022-08-04 17:33:07 -0300

    Creados componentes de uso común
    Se crean componentes:
    - Footer.js
    - Navbar.js
    - Header.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from '../Components/Common/Header';
+import Navbar from '../Components/Common/Navbar';
+import Footer from '../Components/Common/Footer';
 
 const Home = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
