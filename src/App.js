3cf497dea3c9b55bfb23c159278906336611d55f
emalderete <nicolas.alderete95@live.com>
2022-08-17 17:26:10 -0300

    Agregados iconos de redes sociales...
    • Se agregan iconos de redes sociales en el Header que redireccionan a la página principal de cada red social respectivamente.
    • Se crea página provisoria "PageNotFound.js" para cubrir solicitudes de recursos no disponibles / requeridos.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import RecoverPassword from './Pages/RecoverPassword';
+import PageNotFound from './Pages/PageNotFound';
 import './App.css';
 import './Responsive.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
           <Route path='/recover' element={<RecoverPassword></RecoverPassword>}></Route>
+          <Route path='/*' element={<PageNotFound></PageNotFound>}></Route>
         </Routes>
       </Router>
     </div>
--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -28,16 +28,20 @@ const Header = () => {
                             <div style={{margin: '0 2vw', width: '0.2rem', height: '2.5rem', backgroundColor: '#ffffff', borderRadius: '5px'}}></div>
                             <button className='registerButton' type='button'>Registrarse</button>
                         </div>
+                        <div className='social'>
+                            <div>
+                                <span style={{fontSize: '70%', marginLeft: '7px', color: '#ffffff', fontFamily: '"ubuntu", sans-serif'}}>Síguenos en:</span>
+                            </div>
+                            <div>
+                                <a className='icons' href='https://www.facebook.com/' rel='noreferrer noopener' target='blank'><i className='fa-brands fa-square-facebook'></i></a>
+                                <a className='icons' href='https://twitter.com/' rel='noreferrer noopener' target='blank'><i className='fa-brands fa-square-twitter'></i></a>
+                                <a className='icons' href='https://www.instagram.com/' rel='noreferrer noopener' target='blank'><i className='fa-brands fa-instagram'></i></a>
+                                <a className='icons' href='https://www.youtube.com/' rel='noreferrer noopener' target='blank'><i className='fa-brands fa-youtube'></i></a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className='modalContainer'>
-                <div className='modal'>
-                    <div className='modalHeader'>
-                        <button className='btn-close'></button>
-                    </div>
-                    <div className='modalBody'>
-                        este es un texto de prueba
+                    <div className='barsButtonContainer'>
+                        <button className='barsButton' type='button'><i className='fa-solid fa-bars'></i></button>
                     </div>
                 </div>
             </div>
