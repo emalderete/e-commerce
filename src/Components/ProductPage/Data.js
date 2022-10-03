dcb46859802668af205e727fd9242e35f8ebb35a
emalderete <nicolas.alderete95@live.com>
2022-10-03 19:40:05 -0300

    Se desarrolla CRUD: (creación de artículos)
    • Se trabaja sobre el CRUD mediante localStorage.
    En esta etapa ya puede crear nuevos items desde la ventana modal de creación, los mismos pueden ser visualizados en el localStorage del navegador.

---
+++ src/Components/ProductPage/Data.js
@@ -0,0 +1,17 @@
+export class constructorNewItem {
+    constructor(
+        item_name,
+        item_price,
+        item_desc,
+        item_cat,
+        item_image1,
+        item_image2
+    ){
+        this.name = item_name;
+        this.price = item_price;
+        this.description = item_desc;
+        this.category = item_cat;
+        this.cover_image = item_image1;
+        this.optional_image = item_image2;
+    }
+};
--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -3,16 +3,58 @@ import Header from '../Components/Common/Header';
 import Navbar from '../Components/Common/Navbar';
 import Footer from '../Components/Common/Footer';
 import '../Components/ProductPage/ProductPage.css';
+import { constructorNewItem } from '../Components/ProductPage/Data';
 
 const ProductPage = () => {
     const [showOverlay, setShowOverlay] = useState(false);
+    var storage_string = JSON.stringify(localStorage);
 
     function overlayNewItemHandler(){
         showOverlay ? setShowOverlay(false) : setShowOverlay(true);
+    };
+
+    function randomNumberGenerator() {
+        return Math.floor(Math.random() * 10000);
     }
 
+    function newItemLoader(){
+        let itemId = 'item' + randomNumberGenerator();
+        let itemName = document.querySelector('#itemName').value;
+        let itemPrice = document.querySelector('#itemPrice').value;
+        let itemDesc = document.querySelector('#itemDesc').value;
+        let itemCat = document.querySelector('#itemCat').value;
+        let itemImage1 = document.querySelector('#itemImage1').value;
+        let itemImage2 = document.querySelector('#itemImage2').value;
+
+        let itemCreated = new constructorNewItem(itemName, itemPrice, itemDesc, itemCat, itemImage1, itemImage2);
+        let itemCreated_string = JSON.stringify(itemCreated);
+
+        if (storage_string.indexOf(itemId) === -1) {
+            localStorage.setItem(itemId, itemCreated_string);
+            console.log('se agregó el nuevo artículo a la base de datos');
+        } else {
+            itemId = 'item' + randomNumberGenerator();
+            localStorage.setItem(itemId, itemCreated_string);
+            console.log('se agregó el nuevo artículo luego de 1 reintento');
+        }
+
+        // function printProducts(){
+        //     if (localStorage === null){
+        //         return (
+        //             <div className='noProducts'>
+        //                 <h5 style={{marginBottom: '2rem'}}>Por ahora no hay productos que mostrar <i className='fa-solid fa-boxes-packing'>a</i></h5>
+        //                 <h5>¡Agrega uno nuevo!</h5>
+        //                 <h5>Pulsa el botón "<i className='fa-solid fa-circle-plus'></i>"</h5>
+        //             </div>
+        //         );
+        //     } else {
+
+        //     };
+        // };
+    };
+
     return (
-        <div>
+        <div style={showOverlay ? {position: 'fixed', width: '100%'} : null}>
             <Header></Header>
             <Navbar></Navbar>
             <div>
@@ -22,34 +64,66 @@ const ProductPage = () => {
                 </div>
                 <div className='productsContainer'>
                     <div className='products'>
-                        <div className='noProducts'>
                         {/* Cuadro de dialogo a mostrar en caso de que no haya cargado ningun producto */}
-                            <h5 style={{marginBottom: '2rem'}}>Por ahora no hay productos que mostrar <i className='fa-solid fa-boxes-packing'>a</i></h5>
-                            <h5>¡Agrega uno nuevo!</h5>
-                            <h5>Pulsa el botón "<i className='fa-solid fa-circle-plus'></i>"</h5>
+                        {
+                            
+                        }
+                        <div className='productsItem'>
+                            <img src='#' alt=''/>
+                            <h3>Titulo del artículo</h3>
+                            <p>Descripción del artículo</p>
                         </div>
-                        <button type='button' className={showOverlay ? 'addButton moveAddButton' : 'addButton'} onClick={overlayNewItemHandler}><i className='fa-solid fa-circle-plus'></i></button>
+                        <button type='button' className='addButton' onClick={overlayNewItemHandler}><i className='fa-solid fa-circle-plus'></i></button>
                     </div>
                 </div>
             </div>
-            <div className={showOverlay ? 'overlayNewItem' : 'overlayNewItem hideOverlayNewItem'}>
-                <div style={{display: 'flex', justifyContent: 'flex-end', margin: '1rem'}}>
-                    <button style={{fontSize: '1.5rem'}} className='btn-close' onClick={overlayNewItemHandler}></button>
-                </div>
-                <h3>Agregar nuevo producto</h3>
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                    <form className='newItemForm'>
-                        <div className='modalInput'>
-                            <label name='mail'>Nombre del producto</label>
-                            <input type='text' required />
+
+            {/* Modal para ingresar un nuevo artículo */}
+
+            <div className={showOverlay ? 'overlayAddItemContainer' : 'overlayAddItemContainer overlayAddItemContainerHide'}>
+                <div className={showOverlay ? 'overlayAddItem' : 'overlayAddItem overlayAddItemHide'}>
+                    <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <button style={{margin: '1rem', fontSize: '1.2rem', position: 'fixed'}} className='btn-close btn-close-white' onClick={overlayNewItemHandler} />
+                    </div>
+                    <h3>Agregar nuevo artículo</h3>
+                    <form className='formAddItem'>
+                        <div className='modalInput inputAddItem'>
+                            <label htmlFor='itemName'>Nombre del producto</label>
+                            <input type='text' name='itemName' id='itemName' required />
                         </div>
-                        <div className='modalInput'>
-                            <label name='mail'>Precio</label>
-                            <input type='text' required />
+                        <div className='modalInput inputAddItem'>
+                            <label htmlFor='itemPrice'>Precio</label>
+                            <div>
+                                <span style={{color: '#ffffff', fontSize: '1.2rem', marginRight: '0.7rem'}}>$</span>
+                                <input style={{width: '35%'}} type='number' name='itemPrice' id='itemPrice' required />
+                            </div>
                         </div>
-                        <div className='modalInput'>
-                            <label name='mail'>Descripción</label>
-                            <input type='text' required />
+                        <div className='modalInput inputAddItem'>
+                            <label htmlFor='itemDesc'>Descripción</label>
+                            <input type='text' name='itemDesc' id='itemDesc' required />
+                        </div>
+                        <div className='modalInput inputAddItem'>
+                            <label htmlFor='itemCat'>Categoría</label>
+                            <select name='itemCat' id='itemCat' placeholder='- Elige una opción -' required>
+                                <option value='moda'>Moda y accesorios</option>
+                                <option value='belleza'>Artículos de Belleza</option>
+                                <option value='tecnologia'>Tecnología</option>
+                                <option value='herramientas'>Herramientas</option>
+                                <option value='hogar'>Hogar y Muebles</option>
+                                <option value='electrodomesticos'>Electrodomésticos</option>
+                                <option value='juguetes'>Juegos y Juguetes</option>
+                            </select>
+                        </div>
+                        <div className='modalInput inputAddItem'>
+                            <label htmlFor='itemImage1'>Imagen de portada (URL)</label>
+                            <input type='url' name='itemImage1' id='itemImage1' required />
+                        </div>
+                        <div className='modalInput inputAddItem'>
+                            <label htmlFor='itemImage2'>Imagen de muestra (URL, opcional)</label>
+                            <input type='url' name='itemImage2' id='itemImage2' />
+                        </div>
+                        <div className='modalInput inputAddItem'>
+                            <button type='button' onClick={newItemLoader}>Cargar</button>
                         </div>
                     </form>
                 </div>
